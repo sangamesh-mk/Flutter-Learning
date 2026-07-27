3540dfac0d71bc8c30e3b4722028f3347af74831
Sangamesh M Kuri <sangameshmkuri94@gmail.com>
2026-07-27 19:46:24 +0530

    Update the hello world section

--- 01-Dart-Programming/001_Hello_World.dart
+++ 01-Dart-Programming/001_Hello_World.dart
@@ -1,1 +1,14 @@
+void main() {
+  print("Hello World!"); //OUTPUT: Hello World!
+  DispayName();
+}
 
+void DispayName() {
+  String name = "SangameshMK";
+  print(name); //OUTPUT: SangameshMK
+  DisplayAge();
+}
+
+void DisplayAge() {
+  print(20); //OUTPUT: 20
+}
