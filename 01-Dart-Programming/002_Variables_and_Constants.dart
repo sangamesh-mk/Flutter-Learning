5c6e25577f5084b69144be9be725b744d16cee4e
Sangamesh M Kuri <sangameshmkuri94@gmail.com>
2026-08-02 20:22:19 +0530

    Update 002_Variables_and_Constants.dart

--- 01-Dart-Programming/002_Variables_and_Constants.dart
+++ 01-Dart-Programming/002_Variables_and_Constants.dart
@@ -2,9 +2,15 @@ void main() {
   var Age = 20;
   print(Age); //OUTPUT: 20
   DisplayName();
+  DisplayJob();
 }
 
 void DisplayName() {
   const Name = "Sangamesh MK";
   print(Name); //OUTPUT: Sangamesh MK
+}
+
+void DisplayJob() {
+  final JobName = "IAS";
+  print(JobName); //OUTPUT: IAS
 }
